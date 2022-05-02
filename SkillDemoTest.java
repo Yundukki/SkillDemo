3b7a7ea421c826c9bfe8c23faef4554d180b7386
Leo Hu <lch001@ucsd.edu>
2022-05-01 21:04:40 -0700

    added a new test file for skill demo as well as the junit libraries

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void multiply() {
+    assertEquals(2, SkillDemo.multiply(2,3));
+    }
+
+}
